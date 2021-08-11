a1d182d15b1cd4b02d300e286f40bb7fa4d2c58c
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2021-08-11 12:34:28 +0200

    carbonara

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,79 +1,81 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Wicook',
+    title: "Wicook",
     htmlAttrs: {
-      lang: 'fr'
+      lang: "fr"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Envie de cuisiner ? Sur Wicook, retrouvez des recettes conçues avec amour !' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Envie de cuisiner ? Sur Wicook, retrouvez des recettes conçues avec amour !"
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/png', href: './favicon.png' },
+      { rel: "icon", type: "image/png", href: "./favicon.png" },
       {
-        rel: 'preconnect',
-        href: 'https://fonts.gstatic.com'
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com"
       },
       {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Open+Sans&display=swap'
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
       }
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    ['nuxt-fontawesome', {
-      component: 'fa', //customize component name
-      imports: [{
-          set: '@fortawesome/free-solid-svg-icons',
-          icons: ['faHome', 'faSearch', 'faCode', 'faPlus']
+    [
+      "nuxt-fontawesome",
+      {
+        component: "fa", //customize component name
+        imports: [
+          {
+            set: "@fortawesome/free-solid-svg-icons",
+            icons: ["faHome", "faSearch", "faCode", "faPlus"]
           },
-          {set: '@fortawesome/free-brands-svg-icons',
-          icons: []
-          },
-          {set: '@fortawesome/free-regular-svg-icons',
-          icons: []
-          },
-      ]
-   }]
+          { set: "@fortawesome/free-brands-svg-icons", icons: [] },
+          { set: "@fortawesome/free-regular-svg-icons", icons: [] }
+        ]
+      }
+    ]
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  },
+  build: {},
 
   generate: {
-		routes: [
-      '/kfc',
-      '/arancini',
-      '/gratin_de_raviole',
-      '/cakosomon',
-      '/omelettetomatefromage',
-      '/gratindepates',
-      '/risottovegetarien',
-      '/pancakepommes'
-		]
-	},
+    routes: [
+      "/kfc",
+      "/arancini",
+      "/gratin_de_raviole",
+      "/cakosomon",
+      "/omelettetomatefromage",
+      "/gratindepates",
+      "/risottovegetarien",
+      "/pancakepommes",
+      "/carbonara"
+    ]
+  },
 
   router: {
-    base: '/wicook/'
+    base: "/wicook/"
   }
-}
+};
